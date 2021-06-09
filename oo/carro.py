c065632f5939e33ead14258cbe1d51b3c0e2ebea
Fernando Machado <fmachado.rj@gmail.com>
2021-06-09 14:49:57 -0300

    Refatorando a classe Direção

--- oo/carro.py
+++ oo/carro.py
@@ -95,6 +95,31 @@
     'Oeste'
 """
 
+NORTE = 'Norte'
+SUL = 'Sul'
+LESTE = 'Leste'
+OESTE = 'Oeste'
+
+
+class Direcao:
+
+    rotacao_a_direita_dct = {
+        NORTE: LESTE, LESTE: SUL, SUL: OESTE, OESTE: NORTE
+    }
+
+    rotacao_a_esquerda_dct = {
+        NORTE: OESTE, LESTE: NORTE, SUL: LESTE, OESTE: SUL
+    }
+
+    def __init__(self):
+        self.valor = NORTE
+
+    def girar_a_direita(self):
+        self.valor = self.rotacao_a_direita_dct[self.valor]
+
+    def girar_a_esquerda(self):
+        self.valor = self.rotacao_a_esquerda_dct[self.valor]
+
 
 class Motor:
 
@@ -107,24 +132,3 @@ class Motor:
     def frear(self):
         self.velocidade -= 2
         self.velocidade = max(0, self.velocidade)
-
-
-class Direcao:
-
-    pontos_cardeais = ['Norte', 'Leste', 'Sul', 'Oeste']
-    direcao = 0
-
-    def __init__(self):
-        self.valor = self.pontos_cardeais[self.direcao]
-
-    def girar_a_direita(self):
-        self.direcao += 1
-        if self.direcao >= len(self.pontos_cardeais):
-            self.direcao = 0
-        self.valor = self.pontos_cardeais[self.direcao]
-
-    def girar_a_esquerda(self):
-        self.direcao -= 1
-        # if self.direcao < 0:
-        #     self.direcao = 0
-        self.valor = self.pontos_cardeais[self.direcao]
