007be0e513d8b78ce1287572558307a66dc9246e
Fernando Machado <fmachado.rj@gmail.com>
2021-06-09 13:33:55 -0300

    Utilizando @staticmethod e @classmethod na criação de métodos de classe

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -10,6 +10,14 @@ class Pessoa:
     def cumprimentar(self):
         return f'Olá {self.nome}'
 
+    @staticmethod
+    def metodo_estatico():
+        return 348
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - olhos: {cls.olhos}'
+
 
 if __name__ == '__main__':
     fernando = Pessoa(nome='Fernando')
@@ -29,3 +37,5 @@ if __name__ == '__main__':
     print(Pessoa.olhos)
     print(celso.olhos)
     print(fernando.olhos)
+    print(Pessoa.metodo_estatico(), celso.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(), celso.nome_e_atributos_de_classe())
