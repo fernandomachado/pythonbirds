782fa0f151d0d00b0176fd1262631cefe48bed27
Fernando Machado <fmachado.rj@gmail.com>
2021-06-09 13:05:23 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,14 +1,20 @@
 class Pessoa:
 
-    def __init__(self, nome=None, idade=40):
+    def __init__(self, *filhos, nome=None, idade=40):
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {self.nome}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Fernando')
-    print(p.nome)
-    print(p.cumprimentar())
+    fernando = Pessoa(nome='Fernando')
+    celso = Pessoa(fernando, nome='Celso', idade=67)
+    print(celso.nome)
+    print(celso.idade)
+    print(celso.filhos)
+    print(celso.cumprimentar())
+    for filho in celso.filhos:
+        print(filho.nome)
